bb4514739f4733c875b6304bfc6c1f1b3391d648
JacuKingu <71521139@continental.edu.pe>
2024-12-04 14:12:50 -0500

    resolucion de problemas backend

--- backend/20240912_COD_index.js
+++ backend/20240912_COD_index.js
@@ -21,6 +21,8 @@ dotenv.config();
 
 const app = express();
 
+app.set('trust proxy', true);
+
 // Configuración de CORS
 const corsOptions = {
     //origin: ['http://localhost:5173','http://localhost:3001','http://localhost:5000','http://localhost:5002','http://localhost:5003'],
